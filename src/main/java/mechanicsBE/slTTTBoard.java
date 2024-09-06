ee1c2e3a2fae0a2d530929378c3aadcb724690d3
irynaolkhovyk <iolkhovyk@csus.edu>
2024-09-05 18:12:03 -0700

    Add Driver and create slTTTBoard.

---
+++ src/main/java/mechanicsBE/slTTTBoard.java
@@ -0,0 +1,68 @@
+package mechanicsBE;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class slTTTBoard
+{
+    char[][] board;
+    public slTTTBoard()
+    {
+        board = new char[][]{{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
+    }
+    public void printBoard()
+    {
+        for (int i = 0; i < 3; i++) {
+            for(int j=0;j<3;j++) {
+                System.out.print(this.board[i][j] + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    private static  boolean checkForEmpty (char[][] board)
+    {
+        for (int i = 0; i < 3; i++) {
+            for(int j=0;j<3;j++) {
+                if(board[i][j] == '-')
+                    return true;
+            }
+        }
+        return false;
+    }
+    public void play()
+    {
+        Scanner sc = new Scanner(System.in);
+        boolean o = true;
+        int row = -1, col = -1;
+        while (checkForEmpty(this.board)) {
+            System.out.print("Enter the row and column for your entry (space seperated): ");
+
+            if (sc.hasNext("q"))
+                break;
+
+            try {
+                row = sc.nextInt();
+                col = sc.nextInt();
+
+                if (row < 0 || row > 2 || col < 0 || col > 2) {
+                    System.out.println("You're out of board.\nEnter number between 0 and 2.\n Try again: ");
+                    continue;
+                }
+
+                if (this.board[row][col] != '-') {
+                    System.out.println("This place is already taken.\n Try again: ");
+                    continue;
+                }
+                this.board[row][col] = o ? 'o' : 'x';
+                o = !o;
+                printBoard();
+            } catch (InputMismatchException ex) {
+                System.out.println("Please enter 2 numbers divided by space (row column).");
+                sc.nextLine();
+            }
+
+        }
+        System.out.println("Game is finished!");
+    }
+}
---
+++ src/main/java/org/example/csc133Driver.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import mechanicsBE.slTTTBoard;
+
+public class csc133Driver {
+    public static void main(String[] args) {
+        slTTTBoard my_board = new slTTTBoard();
+
+        my_board.printBoard();
+        my_board.play();
+
+        System.out.println("Thank you for playing! Come back to waste more of your time!");
+    }
+}  // public class csc133Driver
+
+
